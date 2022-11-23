8c4cd0206d859d1f96331f0dc698f63c52ab9cbd
Timo <timo.fischer@protonmail.com>
2022-11-24 00:40:16 +0100

    Basic file for describing prime factors

---
+++ prime_factors.py
@@ -0,0 +1,13 @@
+def get_prime_factors(number):
+    factors = []
+    divisor = 2
+    while divisor <= number:
+        if number % divisor == 0:
+            factors.append(divisor)
+            number = number // divisor
+        else:
+            divisor += 1
+    return factors
+
+if __name__ == '__main__':
+    print(get_prime_factors(63))  
